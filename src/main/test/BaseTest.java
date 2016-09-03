685b82807b86d9274da3dd26906651f87d943bfc
jackie19870917 <bim12121@163.com>
2016-09-03 10:32:22 +0800

    init

---
+++ src/main/test/BaseTest.java
@@ -0,0 +1,11 @@
+import org.junit.*;
+/**
+ * Created by Administrator on 2016/9/3.
+ */
+public abstract class BaseTest {
+
+    @Test
+    public void test(){
+
+    }
+}
---
+++ src/main/test/UtilTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+/**
+ * Created by Administrator on 2016/9/3.
+ */
+public class UtilTest extends BaseTest {
+
+    @Override
+    public void test() {
+        System.out.println("test class");
+    }
+}
